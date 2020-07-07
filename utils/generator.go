99b3c9acb37f678f10f2b0786d9f79f10f86fd91
ratanphayade <ratanphayade@gmail.com>
2020-07-07 16:26:21 +0530

    utils: generators added

---
+++ utils/generator.go
@@ -0,0 +1,27 @@
+package utils
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+type GeneratorFunc func(n int) string
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func GenerateString(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
+
+func GenerateInt(n int) string {
+	return fmt.Sprintf("%d", rand.Int63n(1e16))[0:n]
+}
